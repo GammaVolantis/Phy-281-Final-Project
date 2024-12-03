02caace2c97c844fa1e485a1d711f7de18011497
Staton Otto <98541089+GammaVolantis@users.noreply.github.com>
2024-12-03 11:37:36 -0600

    Fixed the vp to nothing

--- FinalProj.py
+++ FinalProj.py
@@ -1,27 +1,29 @@
-import vpython as vp
+import os
+from vpython import *
 #initialize the global variables
 dt = 0.01
-vp.scene.camera.pos = vp.vec(0,1.5,-4.5)
+
+scene.camera.pos = vec(0,1.5,-4.5)
 
 # A 14 lb (6.35 kg) Bowling Ball with 10 initial velocity
-ball = vp.sphere(pos=vp.vec(0,0,0), vel=vp.vec(10,0,0), mass=6.35, radius=.04)
-ball.pos = vp.vec(0,.6,9.144)
-ball.vel= vp.vec(0,0,-00.5)
+ball = sphere(pos=vec(0,0,0), vel=vec(1,0,0), mass=6.35, radius=.04)
+ball.pos = vec(0,.6,9.144)
+ball.vel= vec(0,0,-00.5)
 
 # Lane (meters)
-lane = vp.box(pos=vp.vec(0,0,0), width=1.0668, height=18.288, color=vp.color.red) 
-lane.rotate(axis=vp.vec(1,0,0), angle=vp.pi/2, origin=lane.pos)
+lane = box(pos=vec(0,0,0), width=1.0668, height=18.288, color=color.red) 
+lane.rotate(axis=vec(1,0,0), angle=pi/2, origin=lane.pos)
 
 
 #MAIN LOOP START
 t = 0
 while True:
-    vp.rate(1/dt)
+    rate(1/dt)
     t = t + dt
     
     # Position Update
     ball.pos = ball.pos + ball.vel * t
-    vp.scene.camera.pos = vp.scene.camera.pos + ball.vel * t
+    scene.camera.pos = scene.camera.pos + ball.vel * t
 
     print("hello world: " + str(t))
 
