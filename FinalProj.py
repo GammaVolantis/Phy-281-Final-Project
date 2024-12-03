12222d63b8fed1e841b71aaaac0a231b2e2f1276
DylanRothbauer <djrothbauer@gmail.com>
2024-12-03 11:24:53 -0600

    Added posistion update and refined ball/camera position for the intended look

--- FinalProj.py
+++ FinalProj.py
@@ -1,9 +1,12 @@
 import vpython as vp
 #initialize the global variables
 dt = 0.01
+vp.scene.camera.pos = vp.vec(0,1.5,-4.5)
 
 # A 14 lb (6.35 kg) Bowling Ball with 10 initial velocity
-ball = vp.sphere(pos=vp.vec(0,0,0), vel=vp.vec(10,0,0), mass=6.35, radius=1)
+ball = vp.sphere(pos=vp.vec(0,0,0), vel=vp.vec(10,0,0), mass=6.35, radius=.04)
+ball.pos = vp.vec(0,.6,9.144)
+ball.vel= vp.vec(0,0,-00.5)
 
 # Lane (meters)
 lane = vp.box(pos=vp.vec(0,0,0), width=1.0668, height=18.288, color=vp.color.red) 
@@ -15,6 +18,11 @@ t = 0
 while True:
     vp.rate(1/dt)
     t = t + dt
+    
+    # Position Update
+    ball.pos = ball.pos + ball.vel * t
+    vp.scene.camera.pos = vp.scene.camera.pos + ball.vel * t
+
     print("hello world: " + str(t))
 
 
