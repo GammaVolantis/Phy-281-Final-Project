927ad5e364589ac1ffea2157e0aae9bcc50a18ed
DylanRothbauer <djrothbauer@gmail.com>
2024-12-03 10:42:22 -0600

    Getting ball and lane in

--- FinalProj.py
+++ FinalProj.py
@@ -1,7 +1,13 @@
 import vpython as vp
 #initialize the global variables
 dt = 0.01
-ball = vp.sphere()
+
+# A 14 lb (6.35 kg) Bowling Ball with 10 initial velocity
+ball = vp.sphere(pos=vp.vec(0,0,0), vel=vp.vec(10,0,0), mass=6.35, radius=1)
+
+# Lane (meters)
+lane = vp.box(pos=vp.vec(0,0,0), width=1.0668, height=18.288, color=vp.color.red) 
+lane.rotate(axis=vp.vec(1,0,0), angle=vp.pi/2, origin=lane.pos)
 
 
 #MAIN LOOP START
