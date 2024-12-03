b0c13226319cc450a6de5f3619bd79f0ec328b38
Staton Otto <98541089+GammaVolantis@users.noreply.github.com>
2024-12-03 13:34:39 -0600

    Got the velocity update function working

--- FinalProj.py
+++ FinalProj.py
@@ -1,11 +1,11 @@
 from vpython import *
 #initialize the global variables
 dt = 0.01
-mew = 0.5
+mew = 0.02
 scene.camera.pos = vec(0,1.5,-4.5)
 
 # A 14 lb (6.35 kg) Bowling Ball with 10 initial velocity
-ball = sphere(pos=vec(0,.6,9.144), vel=vec(0,0,-0.005), mass=6.35, radius=.04, make_trail=True, trail_radius=0.02, retain = 35, omega =vec(0,0,0))
+ball = sphere(pos=vec(0,.6,9.144), vel=vec(0,0,-5.36), mass=6.35, radius=.04, make_trail=True, trail_radius=0.02, retain = 35, omega =vec(0,.5,0))
 
 # Lane (meters)
 lane = box(pos=vec(0,0,0), width=1.0668, height=18.288, color=color.red) 
@@ -21,11 +21,14 @@ def velocityRotationUpdate(b):
     #change in velocity
     b.vel = b.vel + (Force/b.mass)*dt
     #change in rotationalvelocity
-    I = (2/3)*b.mass*b.radius**2
+    I = (2/5)*b.mass*b.radius**2
     deltButt = (Torque/I)*dt
     #change in r
     b.pos = b.pos + b.vel*dt
     b.rotate(axis=hat(deltButt),angle=mag(deltButt)*dt)
+def velupdate(b):
+    b.pos=b.pos+b.vel*dt
+    return b
 
 #MAIN LOOP START
 t = 0
@@ -34,10 +37,12 @@ while True:
     t = t + dt
     
     # Position Update
+    #velupdate(ball)
     velocityRotationUpdate(ball)
+    #ball.pos=ball.pos+ball.vel*dt
     scene.camera.pos = scene.camera.pos + ball.vel * dt
 
-    print("hello world: " + str(t))
+    print("velocity = " + str(ball.vel))
 
 
 # MAIN LOOP END
